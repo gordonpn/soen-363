b97ccbe8c5c549ccc505d0385988a73b3c7b58e9
DavidLiang01 <60051944+DavidLiang01@users.noreply.github.com>
2021-04-15 11:53:45 -0400

    refactor

--- phase-2/nosql/7.js
+++ phase-2/nosql/7.js
@@ -1,10 +1,65 @@
 db = new Mongo().getDB("soen-363");
 db.auth("example", "example");
 
-const num_days_2019=365
-const collisions_2019=db.collisions.find({collision_date:{$regex:/^(2019).*/}}).toArray()
-const collisions_2019_Christmas_week=db.collisions.find({collision_date:{$gte:"2019-12-18", $lte:"2019-12-25"}}).toArray()
+const d = new Date();
+const collisions = db.collisions
+  .aggregate([
+    {
+      $match: {
+        collision_date: { $regex: /^(2019).*/ },
+      },
+    },
+    {
+      $facet: {
+        collisions_2019: [
+          {
+            $count: "collisions_2019",
+          },
+        ],
+        collisions_Christmas_2019: [
+          {
+            $match: {
+              collision_date: { $gte: "2019-12-18", $lte: "2019-12-25" },
+            },
+          },
+          {
+            $count: "holidays",
+          },
+        ],
+      },
+    },
+    {
+      $project: {
+        "Daily average accidents in year 2019": {
+          $round: [
+            {
+              $divide: [
+                { $arrayElemAt: ["$collisions_2019.collisions_2019", 0] },
+                365,
+              ],
+            },
+            2,
+          ],
+        },
+        "Daily average accidents in week of Christmas 2019": {
+          $round: [
+            {
+              $divide: [
+                { $arrayElemAt: ["$collisions_Christmas_2019.holidays", 0] },
+                7,
+              ],
+            },
+            2,
+          ],
+        },
+      },
+    },
+  ])
+  .toArray();
 
-print("Is there a higher number of accidents during 2019 holidays (week of Christmas)?")
-print("Daily average accidents in year 2019: "+(collisions_2019.length/num_days_2019).toFixed(2))
-print("Daily average accidents in week of Christmas 2019: "+(collisions_2019_Christmas_week.length/7).toFixed(2))
+printjson({
+  query:
+    "Is there a higher number of accidents during 2019 holidays (week of Christmas)?",
+  result: collisions[0],
+  "execution time": `${new Date() - d} ms`,
+});
