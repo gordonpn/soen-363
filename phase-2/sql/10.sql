2d3187d2943654a348bba667a09e56f7ad927566
WanProduction <48234317+WanProduction@users.noreply.github.com>
2021-04-15 01:20:25 -0400

    Create 10.sql

---
+++ phase-2/sql/10.sql
@@ -0,0 +1,8 @@
+top 10 victim ages that had the most accidents in desc order
+
+SELECT victim_age, COUNT(*) total
+FROM victims
+WHERE victim_age IS NOT null
+GROUP BY victim_age
+ORDER BY total DESC
+LIMIT 10
