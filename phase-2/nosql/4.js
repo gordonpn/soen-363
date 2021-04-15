bd773df944843ffd49a55fedcecdc99f222fb91e
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2021-04-14 20:21:53 -0400

    another query

---
+++ phase-2/nosql/4.js
@@ -0,0 +1,28 @@
+db = new Mongo().getDB("soen-363");
+db.auth("example", "example");
+
+const query = db.collisions
+  .aggregate([
+    {
+      $match: {
+        road_surface: { $in: ["slippery", "snowy", "wet"] },
+      },
+    },
+    {
+      $group: {
+        _id: {
+          pedestrian: "$pedestrian_action",
+          severity: "$collision_severity",
+        },
+        count: { $sum: 1 },
+      },
+    },
+    { $sort: { count: -1 } },
+  ])
+  .toArray();
+
+printjson({
+  query:
+    "Are pedestrians usually involved, and how severe is the collision when the road surface is slippery, snowy, or wet?",
+  result: query.slice(0, 5),
+});
