ab28aefac4850a2ed9907fe8175e05bd60622d1a
WanProduction <48234317+WanProduction@users.noreply.github.com>
2021-04-15 00:58:58 -0400

    Update 1.sql

--- phase-2/sql/1.sql
+++ phase-2/sql/1.sql
@@ -2,4 +2,4 @@ Victim id of all males under 25 that have been killed in an accident that are of
 
 SELECT v.id 
 FROM victims v, parties p 
-WHERE v.victim_sex = 'male' and v.victim_age < 25 and v.victim_degree_of_injury = 'killed' and v.case_id = p.case_id and p.party_race = 'black'
+WHERE v.victim_sex = 'male' and v.victim_age < 25 and v.victim_degree_of_injury = 'killed' and v.case_id = p.case_id and p.party_race = 'black' and v.party_number = p.party_number
