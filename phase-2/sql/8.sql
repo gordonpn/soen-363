b18adc5de5c557aea87a3806f7b6d8906faf582a
Mackenzie Bellemore <43047923+Mackbellemore@users.noreply.github.com>
2021-04-16 11:52:07 -0400

    8 and 9 (#55)
    * 8 and 9
    
    * dadafdsafczc
    
    * fixy

---
+++ phase-2/sql/8.sql
@@ -0,0 +1,18 @@
+-- How many people by race, were killed by a hit and run?
+
+SELECT
+	count(*) AS KILLED_BY_HIT_AND_RUN, p.party_race 
+FROM
+	victims v
+JOIN collisions c ON
+	c.case_id = v.case_id
+JOIN parties p ON
+	p.party_number = v.party_number AND p.case_id = v.case_id 
+WHERE
+	v.victim_degree_of_injury = 'killed'
+	AND c.hit_and_run IN ('misdemeanor', 'felony')
+	AND p.party_race IS NOT NULL
+GROUP BY
+	p.party_race
+ORDER BY 
+	KILLED_BY_HIT_AND_RUN DESC
