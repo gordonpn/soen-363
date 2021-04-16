6c0242c968e814b4fc41aa65d0c6068007fbe417
WanProduction <48234317+WanProduction@users.noreply.github.com>
2021-04-15 21:42:50 -0400

    Formatting

--- phase-2/sql/2.sql
+++ phase-2/sql/2.sql
@@ -1,5 +1,9 @@
 -- How many were uninjured in the passenger seat after being fully ejected 
-
-SELECT COUNT(*)  
-FROM victims v
-WHERE v.victim_role = '2' and v.victim_degree_of_injury = 'no injury' and v.victim_ejected = '1'
+SELECT
+    COUNT(*)
+FROM
+    victims v
+WHERE
+    v.victim_role = '2'
+    and v.victim_degree_of_injury = 'no injury'
+    and v.victim_ejected = '1'
--- phase-2/sql/3.sql
+++ phase-2/sql/3.sql
@@ -1,5 +1,10 @@
 -- How many were female drivers under 21 were severely injured 
-
-SELECT COUNT(*) 
-FROM victims v
-WHERE v.victim_role = '1' and v.victim_sex = 'female' and v.victim_degree_of_injury = '5' and v.victim_age < 21
+SELECT
+    COUNT(*)
+FROM
+    victims v
+WHERE
+    v.victim_role = '1'
+    and v.victim_sex = 'female'
+    and v.victim_degree_of_injury = '5'
+    and v.victim_age < 21
